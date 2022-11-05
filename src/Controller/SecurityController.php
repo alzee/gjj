9ca4aa644e9c3c716071ac76551323710279b2cd
Al Zee <z@alz.ee>
2022-11-05 21:18:09 +0800

    nth

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -22,8 +22,20 @@ class SecurityController extends AbstractController
         $this->doctrine = $doctrine;
     }
 
-    #[Route(path: '/admin/login', name: 'admin_login')]
+    #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    #[Route(path: '/admin/login', name: 'admin_login')]
+    public function adminLogin(AuthenticationUtils $authenticationUtils): Response
     {
         // if ($this->getUser()) {
         //     return $this->redirectToRoute('target_path');
