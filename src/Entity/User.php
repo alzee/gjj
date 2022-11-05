84f0c00dc7abcd1fe901362914d7235316746de8
Al Zee <z@alz.ee>
2022-11-05 18:10:36 +0800

    user fields

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -28,6 +28,72 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?string $password = null;
 
+    #[ORM\Column]
+    private ?int $balance = null;
+
+    #[ORM\Column]
+    private ?int $base = null;
+
+    #[ORM\Column]
+    private ?float $selfRatio = null;
+
+    #[ORM\Column]
+    private ?float $compRatio = null;
+
+    #[ORM\Column]
+    private ?int $selfMonth = null;
+
+    #[ORM\Column]
+    private ?int $compMonth = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $company = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $companyAccount = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $account = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $district = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $bank = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $status = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $startAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $endAt = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $bindBank = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $bankAccount = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?bool $sex = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $idNo = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $birthAt = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $phone = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -96,5 +162,269 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getBalance(): ?int
+    {
+        return $this->balance;
+    }
+
+    public function setBalance(int $balance): self
+    {
+        $this->balance = $balance;
+
+        return $this;
+    }
+
+    public function getBase(): ?int
+    {
+        return $this->base;
+    }
+
+    public function setBase(int $base): self
+    {
+        $this->base = $base;
+
+        return $this;
+    }
+
+    public function getSelfRatio(): ?float
+    {
+        return $this->selfRatio;
+    }
+
+    public function setSelfRatio(float $selfRatio): self
+    {
+        $this->selfRatio = $selfRatio;
+
+        return $this;
+    }
+
+    public function getCompRatio(): ?float
+    {
+        return $this->compRatio;
+    }
+
+    public function setCompRatio(float $compRatio): self
+    {
+        $this->compRatio = $compRatio;
+
+        return $this;
+    }
+
+    public function getSelfMonth(): ?int
+    {
+        return $this->selfMonth;
+    }
+
+    public function setSelfMonth(int $selfMonth): self
+    {
+        $this->selfMonth = $selfMonth;
+
+        return $this;
+    }
+
+    public function getCompMonth(): ?int
+    {
+        return $this->compMonth;
+    }
+
+    public function setCompMonth(int $compMonth): self
+    {
+        $this->compMonth = $compMonth;
+
+        return $this;
+    }
+
+    public function getCompany(): ?string
+    {
+        return $this->company;
+    }
+
+    public function setCompany(?string $company): self
+    {
+        $this->company = $company;
+
+        return $this;
+    }
+
+    public function getCompanyAccount(): ?string
+    {
+        return $this->companyAccount;
+    }
+
+    public function setCompanyAccount(?string $companyAccount): self
+    {
+        $this->companyAccount = $companyAccount;
+
+        return $this;
+    }
+
+    public function getAccount(): ?string
+    {
+        return $this->account;
+    }
+
+    public function setAccount(?string $account): self
+    {
+        $this->account = $account;
+
+        return $this;
+    }
+
+    public function getDistrict(): ?string
+    {
+        return $this->district;
+    }
+
+    public function setDistrict(?string $district): self
+    {
+        $this->district = $district;
+
+        return $this;
+    }
+
+    public function getBank(): ?string
+    {
+        return $this->bank;
+    }
+
+    public function setBank(?string $bank): self
+    {
+        $this->bank = $bank;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getStartAt(): ?\DateTimeImmutable
+    {
+        return $this->startAt;
+    }
+
+    public function setStartAt(?\DateTimeImmutable $startAt): self
+    {
+        $this->startAt = $startAt;
+
+        return $this;
+    }
+
+    public function getEndAt(): ?\DateTimeImmutable
+    {
+        return $this->endAt;
+    }
+
+    public function setEndAt(?\DateTimeImmutable $endAt): self
+    {
+        $this->endAt = $endAt;
+
+        return $this;
+    }
+
+    public function getBindBank(): ?string
+    {
+        return $this->bindBank;
+    }
+
+    public function setBindBank(?string $bindBank): self
+    {
+        $this->bindBank = $bindBank;
+
+        return $this;
+    }
+
+    public function getBankAccount(): ?string
+    {
+        return $this->bankAccount;
+    }
+
+    public function setBankAccount(?string $bankAccount): self
+    {
+        $this->bankAccount = $bankAccount;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function isSex(): ?bool
+    {
+        return $this->sex;
+    }
+
+    public function setSex(bool $sex): self
+    {
+        $this->sex = $sex;
+
+        return $this;
+    }
+
+    public function getIdNo(): ?string
+    {
+        return $this->idNo;
+    }
+
+    public function setIdNo(?string $idNo): self
+    {
+        $this->idNo = $idNo;
+
+        return $this;
+    }
+
+    public function getBirthAt(): ?\DateTimeImmutable
+    {
+        return $this->birthAt;
+    }
+
+    public function setBirthAt(?\DateTimeImmutable $birthAt): self
+    {
+        $this->birthAt = $birthAt;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
     }
 }
