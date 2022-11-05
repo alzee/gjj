e0cbf02a3d4d5970c6b82991ef2bb313037fd1a8
Al Zee <z@alz.ee>
2022-11-05 18:36:35 +0800

    nth

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use App\Entity\Consumer;
+use Doctrine\Persistence\ManagerRegistry;
+
+class SecurityController extends AbstractController
+{
+    private $doctrine;
+    private $client;
+
+    public function __construct(ManagerRegistry $doctrine, HttpClientInterface $client)
+    {
+        $this->httpclient = $client;
+        $this->doctrine = $doctrine;
+    }
+
+    #[Route(path: '/admin/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    #[Route(path: '/admin/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
