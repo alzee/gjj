9e9bd8d3aa75f711ba62d4d545ca52b0597d5cd9
Al Zee <z@alz.ee>
2022-11-06 20:17:16 +0800

    nth

--- public/js/z.js
+++ public/js/z.js
@@ -1,14 +1,21 @@
 let menus = document.querySelectorAll('.fund_grid');
 
-for (let i of menus) {
-  i.addEventListener('click', navTo
-    //(e) => {
-    //  console.log(e)
-    //}
-  )
-};
+let myAcct = document.querySelector('#myAcct');
+if (myAcct) {
+  myAcct.addEventListener('click', navToAccountInfo)
+}
+
+if (menus) {
+  for (let i of menus) {
+    i.addEventListener('click', navTo)
+  };
+}
 
 function navTo(){
   let href = this.dataset.url;
   location.pathname = href;
+}
+
+function navToAccountInfo(){
+  location.pathname = '/account/info';
 }
