86469496808ca26a07866ec62433239e2e37039c
Al Zee <z@alz.ee>
2022-11-07 09:58:56 +0800

    command addAdmin

---
+++ src/Command/AddAdminCommand.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+
+#[AsCommand(
+    name: 'addAdmin',
+    description: 'Add a short description for your command',
+)]
+class AddAdminCommand extends Command
+{
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('username', InputArgument::REQUIRED, 'Argument description')
+            // ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $arg1 = $input->getArgument('username');
+
+
+        if ($arg1) {
+            $io->note(sprintf('You passed an argument: %s', $arg1));
+            $user = new User();
+            $user->setUsername($arg1);
+            $user->setIsAdmin(true);
+            $this->em->persist($user);
+            $this->em->flush();
+        }
+
+        if ($input->getOption('option1')) {
+            // ...
+        }
+
+        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+
+        return Command::SUCCESS;
+    }
+}
